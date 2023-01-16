5e6727c9cadb137b9289016d705afd6abe84264f
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2023-01-16 17:14:48 +0530

    Pages and Routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
 
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-     <h1>This  is a react project</h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/forget-password" element={<ForgotPassword/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
