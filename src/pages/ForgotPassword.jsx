6569dc218c37e7790cb0d2dd6fe2fedc0a843094
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2023-03-08 22:11:32 +0530

    Forgot Password functionality added

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,7 @@
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import OAuth from "../components/OAuth";
 
 export default function SignIn() {
@@ -17,6 +19,18 @@ export default function SignIn() {
     }));
   };
 
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+
+      await sendPasswordResetEmail(auth,email);
+      toast.success("Reset link sent successfully!")
+    } catch (error) {
+      toast.error(error.code);
+    }
+    
+  }
 
   return (
     <section>
@@ -30,7 +44,7 @@ export default function SignIn() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
             <input
               type="email"
@@ -46,12 +60,13 @@ export default function SignIn() {
               <p>Don't have an account?<Link to="/sign-up" className="ml-1 text-red-400 hover:text-red-500">Register</Link></p>
               <Link to="/sign-in" className="text-blue-500 hover:text-blue-700">Sign In instead</Link>
             </div>
-          </form>
+          
           <button type="submit" className="w-full bg-blue-500 hover:bg-blue-600 text-center text-white uppercase font-medium px-6 py-2 rounded transition-all ease-in-out">Reset Password</button>
           <div className="my-5  items-center before:border-t flex before:flex-1 before:border-gray-300 after:border-t  after:flex-1 after:border-gray-300">
           <p className="text-center font-semibold mx-4">OR</p>
           </div>
           <OAuth/>
+          </form>
         </div>
       </div>
     </section>
