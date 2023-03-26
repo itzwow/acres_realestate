edbb35db9b14f89f504a8ed9d966d82b75383abc
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2023-03-26 15:22:47 +0530

    Added map using leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,7 +4,8 @@ import { useParams } from "react-router";
 import Spinner from "../components/Spinner";
 import { db } from "../firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
+import { MapContainer,Popup,TileLayer,Marker } from "react-leaflet";
 import SwiperCore, {
   Navigation,
   Pagination,
@@ -14,10 +15,10 @@ import SwiperCore, {
   Autoplay,
 } from "swiper";
 import "swiper/css/bundle";
-import {FaBath, FaParking, FaShare} from "react-icons/fa"
+import { FaBath, FaParking, FaShare } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { MdChair, MdLocationOn } from "react-icons/md";
-import {FaBed} from "react-icons/fa"
+import { FaBed } from "react-icons/fa";
 import ContactLandlord from "./ContactLandlord";
 
 export default function Listing() {
@@ -27,7 +28,7 @@ export default function Listing() {
   const [linkCopied, setlinkCopied] = useState(false);
   const [contactLandlord, setContactLandlord] = useState(false);
 
-  const auth = getAuth()
+  const auth = getAuth();
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   useEffect(() => {
     const fetchListing = async () => {
@@ -46,9 +47,9 @@ export default function Listing() {
     return <Spinner />;
   }
 
-  const onChange = ()=>{
+  const onChange = () => {
     setContactLandlord(true);
-  }
+  };
   return (
     <main>
       <Swiper
@@ -57,7 +58,6 @@ export default function Listing() {
         navigation
         modules={[EffectFade]}
         autoplay={{ delay: 3000 }}
-        
       >
         {listing.imgUrls.map((url, index) => (
           <SwiperSlide key={index}>
@@ -70,64 +70,105 @@ export default function Listing() {
             ></div>
           </SwiperSlide>
         ))}
-        
       </Swiper>
-      <div className="fixed top-[13%] right-[3%] z-10 cursor-pointer border bg-white w-10 h-10 rounded-full flex justify-center items-center" onClick={()=>{
-        navigator.clipboard.writeText(window.location.href);
-        setlinkCopied(true);
-        setTimeout(()=>{
+      <div
+        className="fixed top-[13%] right-[3%] z-10 cursor-pointer border bg-white w-10 h-10 rounded-full flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setlinkCopied(true);
+          setTimeout(() => {
             toast.success("URL copied successfully!");
             setlinkCopied(false);
-        },10)
-        
-      }}>
-        <FaShare className="text-lg"/>
-        
-        </div>
+          }, 10);
+        }}
+      >
+        <FaShare className="text-lg" />
+      </div>
 
-        <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 bg-white rounded-lg lg:space-x-5">
-            <div className=" w-full ">
-              <p className="text-2xl font-semibold text-blue-900">{listing.name} - ${listing.offer
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 bg-white rounded-lg lg:space-x-5">
+        <div className=" w-full ">
+          <p className="text-2xl font-semibold text-blue-900">
+            {listing.name} - $
+            {listing.offer
               ? listing.discountedPrice
                   .toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
               : listing.regularPrice
                   .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                  {listing.type === "rent"? "/ month":""}
-                  <p className="flex items-center mt-3">
-                    <MdLocationOn className="h-4 w-4 text-green-600" />
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
+          {listing.type === "rent" ? "/ month" : ""}
+          <p className="flex items-center mt-3">
+            <MdLocationOn className="h-4 w-4 text-green-600" />
             <p className="font-semibold  text-sm mb-[2px]  truncate">
               {listing.address}
-                  
-            </p></p>
-            <div className="flex items-center space-x-4 mt-2">
-              <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-md text-center text-white">{listing.type==="Rent"?"For Rent": "For Sale"}</p>
-              <p className="bg-green-800 w-full max-w-[200px] rounded-md p-1 text-md text-center text-white">{listing.offer? `$${+listing.regularPrice - +listing.discountedPrice} discount` :""}</p>
+            </p>
+          </p>
+          <div className="flex items-center space-x-4 mt-2">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-md text-center text-white">
+              {listing.type === "Rent" ? "For Rent" : "For Sale"}
+            </p>
+            <p className="bg-green-800 w-full max-w-[200px] rounded-md p-1 text-md text-center text-white">
+              {listing.offer
+                ? `$${
+                    +listing.regularPrice - +listing.discountedPrice
+                  } discount`
+                : ""}
+            </p>
+          </div>
+          <div className="mt-3 mb-3">
+            <p className="">
+              <span className="font-semibold">Description</span> -{" "}
+              {listing.description}
+            </p>
+          </div>
+          <ul className="flex items-center justify-between">
+            <li className="flex items-center space-x-3">
+              <FaBed className="mr-2 text-xl" />{" "}
+              {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center space-x-3">
+              <FaBath className="mr-2 text-xl" />{" "}
+              {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center space-x-3">
+              <FaParking className="mr-2 text-xl" />{" "}
+              {listing.parking ? "Parking Available" : "Parking Unavailable"}
+            </li>
+            <li className="flex items-center space-x-3">
+              <MdChair className="mr-2 text-xl" />{" "}
+              {listing.furnished ? "Furnished" : "Not Furnished"}
+            </li>
+          </ul>
+          {!contactLandlord && auth.currentUser !== listing.userRef && (
+            <div className="mt-5 mb-3 p-4">
+              <button
+                onClick={onChange}
+                className="w-full bg-blue-500 p-2 rounded"
+              >
+                Contact Landlord
+              </button>
             </div>
-            <div className="mt-3 mb-3">
-              <p className=""><span className="font-semibold">Description</span> - {listing.description}</p>
-            </div>
-            <ul className="flex items-center justify-between">
-              <li className="flex items-center space-x-3"><FaBed className="mr-2 text-xl"/>  {listing.bedrooms>1? `${listing.bedrooms} Beds`: "1 Bed"}</li>
-              <li className="flex items-center space-x-3"><FaBath className="mr-2 text-xl"/>  {listing.bathrooms>1? `${listing.bathrooms} Baths`: "1 Bath"}</li>
-              <li className="flex items-center space-x-3"><FaParking className="mr-2 text-xl"/>  {listing.parking? "Parking Available": "Parking Unavailable"}</li>
-              <li className="flex items-center space-x-3"><MdChair className="mr-2 text-xl"/>  {listing.furnished? "Furnished": "Not Furnished"}</li>
-            </ul>
-            {!contactLandlord  && auth.currentUser !== listing.userRef &&  (<div className="mt-5 mb-3 p-4">
-            <button onClick={onChange} className="w-full bg-blue-500 p-2 rounded">Contact Landlord</button>
-            </div>
-            
-            )
-           
-           }
-           {contactLandlord && <ContactLandlord userRef = {listing.userRef} listing = {listing}/>}
-           
-           </div>
-            <div className="bg-blue-300 w-full h-[200px] z-10 overflow-x-hidden lg-[400px]"></div>
+          )}
+          {contactLandlord && (
+            <ContactLandlord userRef={listing.userRef} listing={listing} />
+          )}
         </div>
-           
-        
+        <div className=" w-full h-[200px] z-10 overflow-x-hidden md:h-[400px]">
+          <MapContainer  center={[+listing.geolocation.lat, +listing.geolocation.lng]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[+listing.geolocation.lat, +listing.geolocation.lng]}>
+              <Popup>
+               {listing.address} <br /> 
+              </Popup>
+            </Marker>
+          </MapContainer>
+          ,
+        </div>
+      </div>
     </main>
   );
 }
